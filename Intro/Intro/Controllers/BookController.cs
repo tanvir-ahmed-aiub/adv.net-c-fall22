22bf126c93c2193daff469e3a673dce1b8783b51
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2022-10-03 16:01:11 +0600

    Validation

---
+++ Intro/Intro/Controllers/BookController.cs
@@ -0,0 +1,39 @@
+﻿using Intro.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Intro.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Book book) {
+            if (ModelState.IsValid) {
+                return RedirectToAction("Index","Home");
+            }
+
+            return View(book);
+        }
+
+        public ActionResult Submit(Book book) {
+            //ViewBag.BookName = Request["Title"];
+            //ViewBag.BookName = form["Title"];
+            ViewBag.BookName = book.Title;
+            
+            return View();
+        }
+    }
+}
---
+++ Intro/Intro/Models/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace Intro.Models
+{
+    public class Book
+    {
+        [Required]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Please provide name")]
+        [StringLength(25,MinimumLength =3,ErrorMessage = "Name should contain 3 t0 25 characters")]
+        public string Author { get; set; }
+        public string Category { get; set; }
+        [Required]
+        [Range(1,20)]
+        public int Copy { get; set; }
+        [Required]
+        [StringLength(50,MinimumLength =8)]
+        public string Password { get; set; }
+        [Compare("Password")]
+        public string ConfPassword { get; set; }
+    }
+}
