f81d4ba17ec28cbf79af8b25715011844c6eccd5
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2022-10-17 15:51:53 +0600

    association

---
+++ Association/Association/Controllers/DepartmentController.cs
@@ -0,0 +1,36 @@
+﻿using Association.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Association.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        // GET: Department
+        public ActionResult Index()
+        {
+            UMSFall22_cEntities db = new UMSFall22_cEntities();
+
+            return View(db.Departments.ToList());
+        }
+        public ActionResult Details(int id) {
+            UMSFall22_cEntities db = new UMSFall22_cEntities();
+            var dept = (from d in db.Departments
+                        where d.Id == id
+                        select d).SingleOrDefault();
+            //var courses = (from c in db.Courses
+            //               where c.DepartmentId == id
+            //               select c).ToList();
+            //var students = (from s in db.Students
+            //                where s.DepartmentId == id
+            //                select s).ToList();
+
+            //ViewBag.courses = courses;
+            //ViewBag.students = students;
+            return View(dept);
+        }
+    }
+}
---
+++ Association/Association/Controllers/RegistrationController.cs
@@ -0,0 +1,34 @@
+﻿using Association.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Association.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        // GET: Registration
+        [HttpGet]
+        public ActionResult Index()
+        {
+            UMSFall22_cEntities db = new UMSFall22_cEntities();
+
+            return View(db.Courses.ToList());
+        }
+        [HttpPost]
+        public ActionResult Index(int[] courses) {
+            UMSFall22_cEntities db = new UMSFall22_cEntities();
+            foreach (var id in courses) {
+                db.CourseStudents.Add(new CourseStudent() { 
+                    CourseId = id,
+                    StudentId = 1,
+
+                });   
+            }
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
